5d56c44a48cdab39da8ceffa66505fff53bc3a2f
sean-carey <sean.carey@ngutucollege.sa.edu.au>
2026-03-08 09:33:11 +1030

    Update test-endpoints.js

--- azure-function/test-endpoints.js
+++ azure-function/test-endpoints.js
@@ -1,24 +1,12 @@
 const auth = require('./shared/veracrossAuth');
-auth.getAccessToken().then(t => {
-  const https = require('https');
-  const paths = [
-    '/v3/students',
-    '/v3/profile-codes',
-    '/v3/student-profiles',
-    '/v3/enrollment-codes',
-    '/v3/code-categories',
-    '/v3/codes',
-    '/v3/profile-code-categories',
-    '/v3/student-profile-code-categories'
-  ];
-  paths.forEach(p => {
-    const req = https.get('https://api.veracross.au/ngutu_college' + p, {
-      headers: { Authorization: 'Bearer ' + t, 'X-Page-Size': '1', 'X-Page-Number': '1' }
-    }, res => {
-      let d = '';
-      res.on('data', c => d += c);
-      res.on('end', () => console.log(p, '->', res.statusCode));
-    });
-    req.end();
-  });
-}).catch(e => console.error(e.message));
+const api = require('./shared/veracrossApi');
+
+auth.getAccessToken().then(token => {
+  return api.fetchProfileCodes(token);
+}).then(result => {
+  console.log('records:', result.records.length);
+  console.log('sample record:', JSON.stringify(result.records[0]));
+  console.log('valueLists:', JSON.stringify(result.valueLists).substring(0, 800));
+}).catch(e => {
+  console.error('ERROR:', e.message);
+});
